7b7dd96445d23612fdafef818d5e06cccd615cfe
AstrouserX <112467284+AstrouserX@users.noreply.github.com>
2023-05-03 17:12:51 +0300

    added tests to TestGoogleClass

--- src/test/java/TestGoogleClass.java
+++ src/test/java/TestGoogleClass.java
@@ -26,7 +26,7 @@ public class TestGoogleClass extends BaseClass{
 5. (Проверка) Нажать на ссылку, содержащую текст "Картинки"
 5. (Ожидаемый результат) Под логотипом отобразился текст "Картинки"
 
-6. (Проверка) Нажать на ссылку, содержащую текст "Почта"
+6. (Проверка) Нажать на кнопку, содержащую текст "Почта"
 6. (Ожидаемый результат) Открылась страница с адресом "https://www.google.com/intl/ru/gmail/about/'
 
 7. (Проверка) Нажать на ссылку, содержащую текст "Всё о Google' в левой нижней части страницы
@@ -39,7 +39,7 @@ public class TestGoogleClass extends BaseClass{
 9. (Ожидаемый результат) Открылась страница с заголовком, содержащим текст "Ваша компания в сервисах Google"
 
 10. (Проверка) Нажать на ссылку, содержащую текст "Как работает Google Поиск" в левой нижней части страницы
-10. (Ожидаемый результат) Открылась страница с заголовком, содержащим текст "Наша цель – систематизировать всю имеющуюся в мире информацию и сделать ее доступной для каждого пользователя"
+10. (Ожидаемый результат) Открылась страница с заголовком, содержащим текст "Наша цель – систематизировать всю имеющуюся в мире информацию и сделать ее доступной для каждого пользователя."
 
 11. (Проверка) Нажать на ссылку, содержащую текст "Конфиденциальность" в правой нижней части страницы
 11. (Ожидаемый результат) Открылась страница содержащая текст "Политика конфиденциальности" в левой верхней части страницы рядом с логотипом
@@ -51,13 +51,13 @@ public class TestGoogleClass extends BaseClass{
         $(byTagAndText(tag, txt)).click();
     }
 
-    @Test(description = "Test1 Как работает кнопка 'Мне повезет'")
+    @Test(description = "Test1 Гость может перейти в календарь событий нажав на кнопку 'Мне повезет'")
     public void testLuckyButton(){
         openUrl(url);
         $(byXpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[2]")).click();
         Assert.assertEquals(WebDriverRunner.getWebDriver().getCurrentUrl(), "https://www.google.com/doodles");
     }
-    @Test(description = "Test2 Как работает ссылка 'Условия'")
+    @Test(description = "Test2 Гость может ознакомиться с условиями использования перейдя по ссылке 'Условия'")
     public void testConditionsUrl(){
         openUrl(url);
         linkTextClick("a","Условия");
@@ -70,46 +70,52 @@ public class TestGoogleClass extends BaseClass{
         ElementsCollection elements = $$(byCssSelector(".LC20lb"));
         Assert.assertEquals(elements.size(), 17);
     }
-    @Test(description = "Test4 Как работает ссылка 'Настройки'")
+    @Test(description = "Test4 Гость может перейти к настройкам браузера нажав на ссылку 'Настройки'")
     public void testSettingsUrl(){
         openUrl(url);
-        //linkTextClick("Настройки");
         $(byTagAndText("div", "Настройки")).click();
         Assert.assertTrue($$(byTagName("g-menu-item")).get(0).text().contains("Настройки поиска"));
-    }/*
-    @Test(description = "Test2 Как работает ссылка 'Условия'")
-    public void test(){
-        openUrl(url);
-        linkTextClick("");
     }
-    @Test(description = "Test2 Как работает ссылка 'Условия'")
-    public void test(){
+    @Test(description = "Test5 Гость может перейти к поиску изображений перейдя по ссылке 'Картинки'")
+    public void testImagesUrl(){
         openUrl(url);
-        linkTextClick("");
+        linkTextClick("a","Картинки");
+        Assert.assertTrue($(byTagAndText("span","Картинки")).exists());
     }
-    @Test(description = "Test2 Как работает ссылка 'Условия'")
-    public void test(){
+    @Test(description = "Test6 Пользователь может перейти в почтовый ящик перейдя по ссылке 'Почта'")
+    public void testGmailUrl(){
         openUrl(url);
-        linkTextClick("");
+        linkTextClick("a", "Почта");
+        Assert.assertTrue(WebDriverRunner.getWebDriver().getCurrentUrl().contains("https://www.google.com/intl/ru/gmail/about/") || WebDriverRunner.getWebDriver().getCurrentUrl().contains("https://accounts.google.com/v3/signin"));
     }
-    @Test(description = "Test2 Как работает ссылка 'Условия'")
-    public void test(){
+    @Test(description = "Test7 Гость может ознакомиться с информацией о компании перейдя по ссылке 'Всё о Google'")
+    public void testAboutUrl(){
         openUrl(url);
-        linkTextClick("");
+        linkTextClick("a","Всё о Google");
+        Assert.assertTrue($(byCssSelector(".glue-grid__col--align-middle > h1")).text().contains("Цель компании"));
     }
-    @Test(description = "Test2 Как работает ссылка 'Условия'")
-    public void test(){
+    @Test(description = "Test8 Гость может ознакомиться с рекламной компанией перейдя по ссылке 'Реклама'")
+    public void testAdsUrl(){
         openUrl(url);
-        linkTextClick("");
+        linkTextClick("a","Реклама");
+        Assert.assertTrue($(byTagAndText("span","Развивайте свою компанию с Google Рекламой")).exists());
     }
-    @Test(description = "Test2 Как работает ссылка 'Условия'")
-    public void test(){
+    @Test(description = "Test9 гость может ознакомиться с условиями для бизнеса перейдя по ссылке 'Для бизнеса'")
+    public void testBusinessUrl(){
         openUrl(url);
-        linkTextClick("");
+        linkTextClick("a","Для бизнеса");
+        Assert.assertEquals($(byCssSelector(".heading")).text(), "Ваша компания в сервисах Google");
     }
-    @Test(description = "Test2 Как работает ссылка 'Условия'")
-    public void test(){
+    @Test(description = "Test10 Гость может ознакомиться с с тем, как работает поисковая система перейдя по ссылке 'Как работает Google Поиск'")
+    public void testSearchWorkUrl(){
         openUrl(url);
-        linkTextClick("");
-    }*/
+        linkTextClick("a", "Как работает Google Поиск");
+        Assert.assertEquals($(byCssSelector(".homepage-hero__content__title")).shouldBe(Condition.visible).text(), "Наша цель – систематизировать всю имеющуюся в мире информацию и сделать ее доступной для каждого пользователя.");
+    }
+    @Test(description = "Test11 Гость может ознакомиться с политикой конфиденциальности перейдя по ссылке 'Конфиденциальность'")
+    public void testConfidentialUrl(){
+        openUrl(url);
+        linkTextClick("a","Конфиденциальность");
+        Assert.assertTrue($(byTagAndText("span","Политика конфиденциальности")).exists());
+    }
 }
