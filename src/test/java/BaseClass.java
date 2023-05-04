2ba2e94fa8086d55f81e3d67c755ec92deed5246
AstrouserX <112467284+AstrouserX@users.noreply.github.com>
2023-05-04 12:52:38 +0300

    add AfterMethod

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -1,10 +1,12 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.webdriver;
 
 public class BaseClass {
     @BeforeTest
@@ -14,10 +16,13 @@ public class BaseClass {
         Configuration.browserSize = "1920x1080";
     }
 
-
-
     @AfterTest
-    public void theEnd(){
+    public void tearDown(){
         Selenide.closeWebDriver();
+    }
+
+    @AfterMethod
+    public void methodEnd(){
+        Selenide.webdriver().driver().close();
     }
 }
